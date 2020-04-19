81d4d74951af7406599f6405266a2d6aa69edea3
Pedro Leinar <pedroleinar@hotmail.com>
2020-04-19 02:08:36 -0300

    finished

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -17,32 +17,27 @@ class ImportTransactionsService {
     const transactionRepository = getCustomRepository(TransactionsRepository);
     const categoriesRepository = getRepository(Category);
 
-    const contactReadStream = fs.createReadStream(filePath);
+    const contactsReadStream = fs.createReadStream(filePath);
 
     const parsers = csvParse({
       from_line: 2,
     });
 
-    const parseCSV = contactReadStream.pipe(parsers);
+    const parseCSV = contactsReadStream.pipe(parsers);
 
     const transactions: TransactionDTO[] = [];
     const categories: string[] = [];
 
     parseCSV.on('data', async line => {
-      const [title, type, value, category] = line.map(
-        (cell: string) => cell.trim,
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
       );
 
       if (!title || !type || !value) return;
 
       categories.push(category);
 
-      transactions.push({
-        title,
-        type,
-        value,
-        category,
-      });
+      transactions.push({ title, type, value, category });
     });
 
     await new Promise(resolve => parseCSV.on('end', resolve));
@@ -62,7 +57,9 @@ class ImportTransactionsService {
       .filter((value, index, self) => self.indexOf(value) === index);
 
     const newCategories = categoriesRepository.create(
-      addCategoryTitles.map(title => ({ title })),
+      addCategoryTitles.map(title => ({
+        title,
+      })),
     );
 
     await categoriesRepository.save(newCategories);
